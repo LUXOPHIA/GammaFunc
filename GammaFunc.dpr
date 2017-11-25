90fa7c5601f2189954d6e8ef3097d1d56041b21d
luxidea <contact@luxidea.net>
2017-11-25 18:02:30 +0900

    begin

---
+++ GammaFunc.dpr
@@ -0,0 +1,17 @@
+﻿program GammaFunc;
+
+uses
+  System.StartUpCopy,
+  FMX.Forms,
+  Main in 'Main.pas' {Form1},
+  LUX.Math.Gamma in '_LIBRARY\LUXOPHIA\LUX\» Math\LUX.Math.Gamma.pas',
+  LUX.Math.Gamma.Lanczos in '_LIBRARY\LUXOPHIA\LUX\» Math\LUX.Math.Gamma.Lanczos.pas',
+  LUX.Math.Gamma.Ooura in '_LIBRARY\LUXOPHIA\LUX\» Math\LUX.Math.Gamma.Ooura.pas';
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
---
+++ Main.pas
@@ -0,0 +1,77 @@
+﻿unit Main;
+
+interface //#################################################################### ■
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
+  FMX.Controls.Presentation, FMX.ScrollBox, FMX.Memo, FMX.StdCtrls, FMX.Edit;
+
+type
+  TForm1 = class(TForm)
+    Memo1: TMemo;
+    Panel1: TPanel;
+      GroupBox1: TGroupBox;
+        LabelX0: TLabel;
+          EditX0: TEdit;
+        LabelX1: TLabel;
+          EditX1: TEdit;
+      LabelN: TLabel;
+        EditN: TEdit;
+      Button1: TButton;
+    procedure Button1Click(Sender: TObject);
+  private
+    { private 宣言 }
+  public
+    { public 宣言 }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation //############################################################### ■
+
+{$R *.fmx}
+
+uses LUX.Math.Gamma.Ooura,
+     LUX.Math.Gamma.Lanczos;
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+
+procedure TForm1.Button1Click(Sender: TObject);
+var
+   N, I :Integer;
+   X0, X1, X, G :Double;
+   S :String;
+begin
+     with Memo1 do
+     begin
+          Lines.Clear;
+
+          N := EditN.Text.ToInteger;
+
+          X0 := EditX0.Text.ToDouble;
+          X1 := EditX1.Text.ToDouble;
+
+          for I := 0 to N do
+          begin
+               X := ( X1 - X0 ) / N * I + X0;
+
+               G := LUX.Math.Gamma.Ooura.Gamma( X );
+
+               if Double.IsInfinity( G ) or Double.IsNan( G ) then S := ''
+                                                              else S := G.ToString;
+
+               Lines.Add( X.ToString + '	' + S );
+          end;
+
+          SelectAll;
+          SetFocus;
+     end;
+end;
+
+end. //######################################################################### ■
