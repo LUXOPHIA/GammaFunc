6ab3dc66164e38ff267ae0f8d68b9832a46d6757
luxidea <contact@luxidea.net>
2017-11-30 06:47:17 +0900

    Squashed '_LIBRARY/LUXOPHIA/LUX/' changes from 4feecac..f323f6a
    f323f6a CumDistT 関数における切り替え条件を X < Roo2( V ) / 10 に設定。
    6908c1d IncBeta 関数の精度を改善。
    3df218d CumDistT 関数における式の切り替え位置を修正。
    da7f241 自由度 N_ → V_ 引数へ変更。
    8f04042 TSingle2D ⇔ TDouble2D 型変換を追加。
    eaeb650 no message
    dc6ff0c 収束判定条件の見直し。
    47e3dff 計算精度を緩和。
    02c41a9 CumDistT 関数の不安定性を除去。
    be32002 ｔ分布 の 累積分布関数/逆関数 CumDistT / InvCumDistT を追加。
    e3fc123 正則/不完全ベータ関数 IncBeta, RegIncBeta を追加。
    b8f6522 自動微分型ガンマ関数を追加。
    059cb71 ガウスの超幾何関数 HypGeo21 を追加。
    47b7b6c 自動微分用 Exp / Ln/ Power 関数を追加。
    86e8de2 イプシロン SINGLE_EPS* 定数を追加。
    ae79510 ｔ分布関数を追加。
    1eb3d46 ベータ関数を追加。
    9144f3d LUX.Math 系ユニットを » Math フォルダへ整理。
    f361f46 LUX.Data 系ユニットを » Data フォルダへ整理。
    98c2d49 LUX.Math ユニットを追加。 ガンマ関数を追加。
    
    git-subtree-dir: _LIBRARY/LUXOPHIA/LUX
    git-subtree-split: f323f6ae94faeacfe4b70745fdec7411c45f593f

---
+++ » Math/LUX.Math.Distribution.pas
@@ -0,0 +1,181 @@
+﻿unit LUX.Math.Distribution;
+
+interface //#################################################################### ■
+
+uses LUX, LUX.D1;
+
+//type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【型】
+
+     //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【レコード】
+
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% {RECORD}
+
+     //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【クラス】
+
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% {CLASS}
+
+//const //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【定数】
+
+//var //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【変数】
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【ルーチン】
+
+function DistT( const X_:Single; const V_:Cardinal ) :Single; overload;
+function DistT( const X_:Double; const V_:Cardinal ) :Double; overload;
+function DistT( const X_:TdSingle; const V_:Cardinal ) :TdSingle; overload;
+function DistT( const X_:TdDouble; const V_:Cardinal ) :TdDouble; overload;
+
+function CumDistT( const X_:Single; const V_:Cardinal ) :Single; overload;
+function CumDistT( const X_:Double; const V_:Cardinal ) :Double; overload;
+function CumDistT( const X_:TdSingle; const V_:Cardinal ) :TdSingle; overload;
+function CumDistT( const X_:TdDouble; const V_:Cardinal ) :TdDouble; overload;
+
+function InvCumDistT( const P_:Single; const V_:Cardinal ) :Single; overload;
+function InvCumDistT( const P_:Double; const V_:Cardinal ) :Double; overload;
+
+implementation //############################################################### ■
+
+uses System.Math,
+     LUX.Math.Special.Beta;
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【レコード】
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% {RECORD}
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【クラス】
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% {CLASS}
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【ルーチン】
+
+function DistT( const X_:Single; const V_:Cardinal ) :Single;
+begin
+     Result := Power( 1 + Pow2( X_ ) / V_, -( V_ + 1 ) / 2 )
+             / ( Roo2( V_ ) * Beta( V_ / 2, 0.5 ) );
+end;
+
+function DistT( const X_:Double; const V_:Cardinal ) :Double;
+begin
+     Result := Power( 1 + Pow2( X_ ) / V_, -( V_ + 1 ) / 2 )
+             / ( Roo2( V_ ) * Beta( V_ / 2, 0.5 ) );
+end;
+
+function DistT( const X_:TdSingle; const V_:Cardinal ) :TdSingle;
+begin
+     Result := Power( 1 + Pow2( X_ ) / V_, -( V_ + 1 ) / 2 )
+             / ( Roo2( V_ ) * Beta( V_ / 2, 0.5 ) );
+end;
+
+function DistT( const X_:TdDouble; const V_:Cardinal ) :TdDouble;
+begin
+     Result := Power( 1 + Pow2( X_ ) / V_, -( V_ + 1 ) / 2 )
+             / ( Roo2( V_ ) * Beta( V_ / 2, 0.5 ) );
+end;
+
+//------------------------------------------------------------------------------
+
+function CumDistT( const X_:Single; const V_:Cardinal ) :Single;
+var
+   X2, B :Double;
+begin
+     X2 := Pow2( X_ );
+
+     //  I( x a, b ) = 1 - I( 1 - x b, a )
+
+     if 100 * X2 < V_ then B := 1 - RegIncBeta( X2 / ( X2 + V_ ), 1  / 2, V_ / 2 )
+                      else B :=     RegIncBeta( V_ / ( X2 + V_ ), V_ / 2, 1  / 2 );
+
+     if X_ < 0 then Result :=     B / 2
+               else Result := 1 - B / 2;
+end;
+
+function CumDistT( const X_:Double; const V_:Cardinal ) :Double;
+var
+   X2, B :Double;
+begin
+     X2 := Pow2( X_ );
+
+     //  I( x a, b ) = 1 - I( 1 - x b, a )
+
+     if 100 * X2 < V_ then B := 1 - RegIncBeta( X2 / ( X2 + V_ ), 1  / 2, V_ / 2 )
+                      else B :=     RegIncBeta( V_ / ( X2 + V_ ), V_ / 2, 1  / 2 );
+
+     if X_ < 0 then Result :=     B / 2
+               else Result := 1 - B / 2;
+end;
+
+function CumDistT( const X_:TdSingle; const V_:Cardinal ) :TdSingle;
+begin
+     with X_ do
+     begin
+          Result.o := CumDistT( o, V_ )    ;
+          Result.d :=    DistT( o, V_ ) * d;
+     end;
+end;
+
+function CumDistT( const X_:TdDouble; const V_:Cardinal ) :TdDouble;
+begin
+     with X_ do
+     begin
+          Result.o := CumDistT( o, V_ )    ;
+          Result.d :=    DistT( o, V_ ) * d;
+     end;
+end;
+
+//------------------------------------------------------------------------------
+
+function InvCumDistT( const P_:Single; const V_:Cardinal ) :Single;
+var
+   X, P :TdSingle;
+   Pd :Single;
+begin
+     X := TdSingle.Create( 0, 1 );
+
+     repeat
+           P := CumDistT( X, V_ );
+
+           Pd := P.o - P_;
+
+           X.o := X.o - Pd / P.d;
+
+     until Abs( Pd ) < SINGLE_EPS3;
+
+     Result := X.o;
+end;
+
+function InvCumDistT( const P_:Double; const V_:Cardinal ) :Double;
+var
+   X, P :TdDouble;
+   Pd :Double;
+begin
+     X := TdDouble.Create( 0, 1 );
+
+     repeat
+           P := CumDistT( X, V_ );
+
+           Pd := P.o - P_;
+
+           X.o := X.o - Pd / P.d;
+
+     until Abs( Pd ) < DOUBLE_EPS3;
+
+     Result := X.o;
+end;
+
+//############################################################################## □
+
+initialization //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ 初期化
+
+finalization //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ 最終化
+
+end. //######################################################################### ■
